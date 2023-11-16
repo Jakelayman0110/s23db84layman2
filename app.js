fc1bd2a1f62807cf42a9fd824989f0f01451f376
Jakelayman0110 <99349809+Jakelayman0110@users.noreply.github.com>
2023-11-15 19:34:37 -0600

    update

--- app.js
+++ app.js
@@ -1,45 +1,58 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var donutRouter = require('./routes/donut');
-var boardRouter = require('./routes/board');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var donutRouter = require("./routes/donut");
+var boardRouter = require("./routes/board");
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+require("dotenv").config();
+const connectionString = process.env.MONGO_CON;
+mongoose = require("mongoose");
+mongoose.connect(connectionString);
 
-app.use(logger('dev'));
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event
+db.on("error", console.error.bind(console, "MongoDB connectionerror:"));
+db.once("open", function () {
+  console.log("Connection to DB succeeded");
+});
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/donut', donutRouter);
-app.use('/board', boardRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/donut", donutRouter);
+app.use("/board", boardRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
