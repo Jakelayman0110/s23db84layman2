cffef13ed05dea822200e7fc8ac6c0e7ba89f401
Jakelayman0110 <99349809+Jakelayman0110@users.noreply.github.com>
2023-11-27 11:37:31 -0600

    update

--- controllers/donut.js
+++ controllers/donut.js
@@ -124,8 +124,8 @@ exports.donut_list = async function (req, res) {
 exports.costume_view_one_Page = async function (req, res) {
   console.log("single view for id " + req.query.id);
   try {
-    result = await Costume.findById(req.query.id);
-    res.render("costumedetail", { title: "Costume Detail", toShow: result });
+    result = await Donut.findById(req.query.id);
+    res.render("donutdetail", { title: "Donut Detail", toShow: result });
   } catch (err) {
     res.status(500);
     res.send(`{'error': '${err}'}`);
--- routes/donut.js
+++ routes/donut.js
@@ -3,6 +3,6 @@ const donut_controller = require("../controllers/donut");
 var router = express.Router();
 
 /* GET */
-router.get("/", donut_controlers.donut_view_all_Page);
+router.get("/", donut_controller.donut_view_all_Page);
 
 module.exports = router;
